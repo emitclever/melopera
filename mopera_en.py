63550636c3b69e5ad35cf644b0d2ed88e8a8a5de
emitclever <adrian@macproair.luparu.local>
2015-03-07 01:45:19 -0500

    my first python program for my daughter first math

---
+++ mopera_en.py
@@ -0,0 +1,29 @@
+#-*-coding:utf8;-*-
+#qpy:2
+#qpy:console
+# My very first program on python for my daughter first math problems.
+# Need qpython3 application for android or mac windows
+# Lets play
+from random import randint
+import random
+correct = 0
+rng = 10
+print("Hello Melissa")
+for i in range (rng):  # (number of questions)
+    n1 = randint(0,6)   # (numbers to choose randomly)
+    n2 = randint(0,n1)
+    opera = random.choice(r'+-') # operations ransom choosing could be '+-*/' depend on complexity
+    resultat = eval(str(n1) + opera + str(n2))
+    print(n1,opera,n2,end="")
+    inp=(input(" = "))
+    if inp.isdigit():
+        ans = int(inp)
+    else:
+        ans = 0
+    if ans == resultat:
+        print("TRUE. :) \n\n")
+        correct = correct + 1
+    else:
+        print("FALSE :( , Answer is %d.\n\n" % resultat)
+print("\n %d / %d." % (rng,correct))
+print("End")
