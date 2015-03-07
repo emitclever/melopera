0d725de9d01fa12b9c362f3fb9a8d546173d6dbc
emitclever <adrian@macproair.luparu.local>
2015-03-07 01:49:59 -0500

    math operation
    my first python program for my daughter first math

--- mopera_en.py
+++ mopera_en.py
@@ -1,8 +1,8 @@
 #-*-coding:utf8;-*-
-#qpy:2
-#qpy:console
+# qpy:2
+# qpy:console
 # My very first program on python for my daughter first math problems.
-# Need qpython3 application for android or mac windows
+# Need qpython3 application for android (python3 mac or windows)
 # Lets play
 from random import randint
 import random
@@ -12,11 +12,11 @@ print("Hello Melissa")
 for i in range (rng):  # (number of questions)
     n1 = randint(0,6)   # (numbers to choose randomly)
     n2 = randint(0,n1)
-    opera = random.choice(r'+-') # operations ransom choosing could be '+-*/' depend on complexity
+    opera = random.choice(r'+-') # random operation choosing could be '+-*/' depend on complexity
     resultat = eval(str(n1) + opera + str(n2))
     print(n1,opera,n2,end="")
     inp=(input(" = "))
-    if inp.isdigit():
+    if inp.isdigit(): # check if valid integer not empty
         ans = int(inp)
     else:
         ans = 0
@@ -27,3 +27,4 @@ for i in range (rng):  # (number of questions)
         print("FALSE :( , Answer is %d.\n\n" % resultat)
 print("\n %d / %d." % (rng,correct))
 print("End")
+
